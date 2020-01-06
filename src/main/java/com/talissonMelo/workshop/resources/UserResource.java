5dafa5be79371be1e24cf7ba12d71e4746262f0e
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-01-06 10:42:14 -0300

    DTO to return users

--- src/main/java/com/talissonMelo/workshop/resources/UserResource.java
+++ src/main/java/com/talissonMelo/workshop/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.talissonMelo.workshop.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.talissonMelo.workshop.domain.User;
+import com.talissonMelo.workshop.dto.UserDTO;
 import com.talissonMelo.workshop.services.UserService;
 
 @RestController
@@ -19,10 +21,11 @@ public class UserResource {
 	private UserService userService;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 
 		List<User> list = userService.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 
 	}
 }
