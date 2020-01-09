cfab01b296ab286c9a1ed46aedc4941245f0015e
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-01-09 09:51:26 -0300

    Insert

--- src/main/java/com/talissonMelo/workshop/resources/UserResource.java
+++ src/main/java/com/talissonMelo/workshop/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.talissonMelo.workshop.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.talissonMelo.workshop.domain.User;
 import com.talissonMelo.workshop.dto.UserDTO;
@@ -34,6 +39,16 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User user = userService.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(user));
+
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+
+		User obj = userService.fromDTO(objDto);
+		obj = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 
 	}
 }
--- src/main/java/com/talissonMelo/workshop/services/UserService.java
+++ src/main/java/com/talissonMelo/workshop/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.talissonMelo.workshop.domain.User;
+import com.talissonMelo.workshop.dto.UserDTO;
 import com.talissonMelo.workshop.repository.UserRepository;
 import com.talissonMelo.workshop.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
