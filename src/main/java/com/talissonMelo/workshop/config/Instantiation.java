680b4f065ec035e6301c8329da21c73d56337eda
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-01-11 13:45:10 -0300

    User Posts

--- src/main/java/com/talissonMelo/workshop/config/Instantiation.java
+++ src/main/java/com/talissonMelo/workshop/config/Instantiation.java
@@ -44,6 +44,8 @@ public class Instantiation implements CommandLineRunner {
 
 		postRepository.saveAll(Arrays.asList(p1, p2));
 
+		u1.getPosts().addAll(Arrays.asList(p1, p2));
+		userRepository.save(u1);
 	}
 
 }
