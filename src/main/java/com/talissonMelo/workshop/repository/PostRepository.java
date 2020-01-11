504b13fe6ca101585ca3280d6ef653b15df27926
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-01-11 17:37:36 -0300

    @Query

--- src/main/java/com/talissonMelo/workshop/repository/PostRepository.java
+++ src/main/java/com/talissonMelo/workshop/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.talissonMelo.workshop.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.talissonMelo.workshop.domain.Post;
@@ -13,6 +14,15 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	// Query Methods
 	// https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/
 	// https://docs.spring.io/spring-data/data-document/docs/current/reference/html/
+
 	List<Post> findByTitleContainingIgnoreCase(String text);
+
+	// @Query
+	// https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/
+	// https://docs.spring.io/spring-data/data-document/docs/current/reference/html/
+	// https://docs.mongodb.com/manual/reference/operator/query/regex/
+
+	@Query("{ 'title': { $regex: ?0 , $options: 'i'} }")
+	List<Post> searchTitle(String text);
 
 }
