3f0484b3c7de86844277c6b2f6daa11f9532cb59
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-01-11 14:17:09 -0300

    Post findById

---
+++ src/main/java/com/talissonMelo/workshop/resources/PostResource.java
@@ -0,0 +1,27 @@
+package com.talissonMelo.workshop.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.talissonMelo.workshop.domain.Post;
+import com.talissonMelo.workshop.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService postService;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post post = postService.findById(id);
+		return ResponseEntity.ok().body(post);
+
+	}
+
+}
---
+++ src/main/java/com/talissonMelo/workshop/services/PostService.java
@@ -0,0 +1,23 @@
+package com.talissonMelo.workshop.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.talissonMelo.workshop.domain.Post;
+import com.talissonMelo.workshop.repository.PostRepository;
+import com.talissonMelo.workshop.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
