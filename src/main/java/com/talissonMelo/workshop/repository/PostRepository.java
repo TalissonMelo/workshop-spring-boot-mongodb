dd99f6438ce08ca10b731f1c5eae7c26fb4704b9
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-01-11 15:01:17 -0300

    Query methods

--- src/main/java/com/talissonMelo/workshop/repository/PostRepository.java
+++ src/main/java/com/talissonMelo/workshop/repository/PostRepository.java
@@ -1,11 +1,18 @@
 package com.talissonMelo.workshop.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.talissonMelo.workshop.domain.Post;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post, String>{
+public interface PostRepository extends MongoRepository<Post, String> {
+
+	// Query Methods
+	// https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/
+	// https://docs.spring.io/spring-data/data-document/docs/current/reference/html/
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
--- src/main/java/com/talissonMelo/workshop/resources/PostResource.java
+++ src/main/java/com/talissonMelo/workshop/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.talissonMelo.workshop.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.talissonMelo.workshop.domain.Post;
+import com.talissonMelo.workshop.resources.util.URL;
 import com.talissonMelo.workshop.services.PostService;
 
 @RestController
@@ -21,6 +25,15 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post post = postService.findById(id);
 		return ResponseEntity.ok().body(post);
+
+	}
+
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findBTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+
+		text = URL.decodeParam(text);
+		List<Post> list = postService.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 
 	}
 
